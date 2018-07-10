cb37b6c379d0470606a63d54e9cbd9a6ca888ff7
tawfiknasser <tawfiknasser@gmail.com>
2018-07-10 17:26:17 +0300

    #1 addTodo Function+ tests

--- skeleton/logic.js
+++ skeleton/logic.js
@@ -1,7 +1,7 @@
 // Part 1. Fill in any missing parts of the todoFunction object!
 // you can access these on todo.todoFunctions
 // For part one we expect you to use tdd
-
+var todos=[];
 var todoFunctions = {
   // todoFunctions.generateId() will give you a unique id
   // You do not need to understand the implementation of this function.
@@ -14,25 +14,36 @@ var todoFunctions = {
 
     return incrementCounter;
   })(),
-  
-  //cloneArrayOfObjects will create a copy of the todos array 
+
+  //cloneArrayOfObjects will create a copy of the todos array
   //changes to the new array don't affect the original
   cloneArrayOfObjects: function(todos) {
     return todos.map(function(todo){
       return JSON.parse(JSON.stringify(todo));
+
     });
   },
-  
-  addTodo: function(todos, newTodo) {
+
+  addTodo: function(todos, newTodo) { //issue number #1
     // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
     // returns a new array, it should contain todos with the newTodo added to the end.
     // add an id to the newTodo. You can use the generateId function to create an id.
     // hint: array.concat
+
+
+newTodo.id=todoFunctions.generateId();
+
+  newArr=todoFunctions.cloneArrayOfObjects(todos);
+  newArr=newArr.concat(newTodo);
+  return newArr;
+
+
   },
   deleteTodo: function(todos, idToDelete) {
     // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
     // return a new array, this should not contain any todo with an id of idToDelete
     // hint: array.filter
+
   },
   markTodo: function(todos, idToMark) {
     // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
@@ -51,7 +62,7 @@ var todoFunctions = {
 
 // Why is this if statement necessary?
 // The answer has something to do with needing to run code both in the browser and in Node.js
-// See this article for more details: 
+// See this article for more details:
 // http://www.matteoagosti.com/blog/2013/02/24/writing-javascript-modules-for-both-browser-and-node/
 if (typeof module !== 'undefined') {
   module.exports = todoFunctions;
--- skeleton/test.js
+++ skeleton/test.js
@@ -1,7 +1,11 @@
 var test = require('tape');
 var logic = require('./logic');
 
-test('Example test', function(t) {
-  t.pass();
+test('addTodo Function logic work',function(t){
+
+  t.deepEqual(logic.addTodo([],{des:"wenak",done:false}),[{ id:1 ,des:"wenak",done:false}],"NOOO");
+  t.deepEqual(logic.addTodo([{ id:1 ,des:"wenak",done:false}],{des:"suha",done:false}),[{ id:1 ,des:"wenak",done:false},{des:"suha",done:false ,id:2}],"NOOO");
+  t.deepEqual(logic.addTodo([{id:2, des:"error", done:"false"}],{des:"hi", done:"yes"}),[{id:2, des:"error", done:"false"},{id:3, des: "hi", done:"yes"}],"yesssss");
+
   t.end();
-});
+})
