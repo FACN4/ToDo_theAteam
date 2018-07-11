a60d29d117c8e22fd7981ef671322f55d474ccfb
tawfiknasser <tawfiknasser@gmail.com>
2018-07-12 01:03:30 +0300

    #7 Added the option to click anywhere on the span to mark as completed

--- skeleton/dom.js
+++ skeleton/dom.js
@@ -28,7 +28,7 @@
     var deleteButtonNode = document.createElement("button");
 
     deleteButtonNode.className="btn-delete";
-
+    deleteButtonNode.innerHTML="X";
     deleteButtonNode.addEventListener("click", function(event) {
       var newState = todoFunctions.deleteTodo(state, todo.id);
       update(newState);
@@ -44,18 +44,13 @@
     }
     todoNode.appendChild(todoSpan);
 
-        // add markTodo button
-        var markTodoButtonNode = document.createElement("button");
-          markTodoButtonNode.className="btn-mark";
         var strike = document.createElement("strike");
-        markTodoButtonNode.addEventListener("click", function(event) {
+        todoSpan.addEventListener("click", function(event) {
           var newState = todoFunctions.markTodo(state, todo.id);
           update(newState);
         });
-        todoNode.appendChild(markTodoButtonNode);
 
-    // add classes for css
-    // Matt this is for you
+
     return todoNode;
   };
 
